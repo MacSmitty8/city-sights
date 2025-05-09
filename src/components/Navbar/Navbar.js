7465d8ed62baafd6290c8b53910175ea70cdbd40
MacreenSmith <macreensmith@gmail.com>
2025-05-08 21:36:29 -0400

    Created a Navbar Component and applied styling to the Navbar.

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,23 @@ export default function Navbar() {
   return (
    <nav className='navbar'>
     <h3 className='logo'>City Sights</h3>
+    <ul className='nav-links'>
+        <li>
+            <a href='/' className='nav-link'>
+                Home
+            </a>
+        </li>
+        <li>
+            <a href='/' className='nav-link'>
+               About 
+            </a>
+        </li>
+        <li>
+            <a href='/' className='nav-link active'>
+              Tours  
+            </a>
+        </li>
+    </ul>
    </nav>
   )
 }
