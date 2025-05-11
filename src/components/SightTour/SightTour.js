3b89fea2e24427a1fd529720b37f0d1bef7f912f
MacreenSmith <macreensmith@gmail.com>
2025-05-11 10:54:29 -0400

    Added a window close button.

--- src/components/SightTour/SightTour.js
+++ src/components/SightTour/SightTour.js
@@ -2,11 +2,17 @@ import React from 'react'
 
 const SightTour = () => {
   return (
-    <div>
-      <section>
-        Tourism
-      </section>
+    <article className='tour'>
+    <div className='image-container'>
+        <img width= "250" 
+        src='https://images.pexels.com/photos/1239162/pexels-photo-1239162.jpeg?auto=compress&cs=tinysrgb&w=600'
+        alt='New York'/>
     </div>
+    <span className="close-btn">
+      <i className="fas fa-window-close" />
+    </span>
+        
+    </article>
   )
 }
 
